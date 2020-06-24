758d710a298b916b1288d3a5732d9cbeafb71f83
fse-dev <elmanoukdesign@gmail.com>
2020-06-24 19:10:41 -0400

    Updates

---
+++ precache-manifest.14a098093781744d7999afc58f6ee72a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2067f5b130bff55cdd68a3d37935acc1",
+    "url": "/fserisen/index.html"
+  },
+  {
+    "revision": "24068af4a6df26f27fc0",
+    "url": "/fserisen/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "24068af4a6df26f27fc0",
+    "url": "/fserisen/static/js/2.24fe55d0.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/fserisen/static/js/2.24fe55d0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "940a6deb4c488abff41c",
+    "url": "/fserisen/static/js/main.5b1d5c8c.chunk.js"
+  },
+  {
+    "revision": "be2028cf2dbbea84e2c7",
+    "url": "/fserisen/static/js/runtime-main.7ce18097.js"
+  }
+]);
