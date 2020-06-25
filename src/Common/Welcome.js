dfbb7253474c27717f2cfacebe2554824992a056
fse-dev <elmanoukdesign@gmail.com>
2020-06-25 02:09:32 -0400

    terminal look typer functionality

--- src/Common/Welcome.js
+++ src/Common/Welcome.js
@@ -2,57 +2,71 @@ import React, { useState, useEffect } from "react";
 import { Container, Jumbotron, Row, Col } from "react-bootstrap";
 import Typewriter from "typewriter-effect";
 
-const Box = ({ terminal, command }) => {
+const useWindowEvent = (event, callback) => {
+  useEffect(() => {
+    window.addEventListener(event, callback);
+    return () => window.removeEventListener(event, callback);
+  }, [event, callback]);
+};
+const useGlobalMouseMove = (callback) => {
+  return useWindowEvent("keypress", callback);
+};
+
+const Box = ({ command, terminal }) => {
   const [flag, setFlag] = useState(true);
-  const [val, setVal] = useState();
-  const navList =
-    "<p> a) Git </p> <br> <p> b) Education</p> <br> <p> c) Skills</p>";
   useEffect(() => {
     setTimeout(() => {
       setFlag(false);
-    }, 5000);
+    }, 8000);
+    return clearTimeout(setFlag(true));
   }, []);
   return (
     <Jumbotron style={styles.terminal}>
-      <div style={styles.terminalText}>{terminal}</div>
       <div style={styles.terminalText}>
-        {flag === true && (
-          <Typewriter
-            options={{ autoStart: true }}
-            onInit={(typewriter) => {
-              typewriter.typeString(command).start();
-            }}
-          />
-        )}
-        {flag !== true && (
-          <Typewriter
-            options={{ autoStart: true }}
-            onInit={(typewriter) => {
-              typewriter
-                .pauseFor(2000)
-                .typeString(".....")
-                .pauseFor(2000)
-                .deleteAll(10)
-                .pasteString(navList)
-                .start();
-            }}
-          />
-        )}
+        <span style={styles.terminalTextSpan}>{terminal[0]}</span>
+      </div>
+      <div style={styles.terminalText}>
+        <Typewriter
+          options={{ autoStart: true }}
+          onInit={(typewriter) => {
+            typewriter.typeString(command).start();
+          }}
+        />
       </div>
     </Jumbotron>
   );
 };
 
 const Header = () => {
-  const terminal = "~$:";
-  const command = "sudo ./sadik_erisen.h ";
+  const [flag, setFlag] = useState(true);
+  const bash = "~$";
+  const user = "guest_user:";
+  const command = " sudo ./fse_dev_OS.sh";
+  useGlobalMouseMove((e) => (e ? setFlag(false) : null));
   return (
     <Container fluid>
       <Row className="justify-content-lg-center">
         <Col xs lg="6">
-          <Box terminal={terminal} command={command} />;
+          <Jumbotron style={styles.terminal}>
+            <div style={styles.terminalText}>
+              <span style={styles.terminalTextSpan}>{bash}</span>
+            </div>
+            <input
+              size="30"
+              maxLength="10"
+              style={styles.inputField}
+              defaultValue={"press any key to start..."}
+            />
+          </Jumbotron>
         </Col>
       </Row>
+      {!flag ? (
+        <Row className="justify-content-lg-center" style={{ marginTop: -150 }}>
+          <Col xs lg="6">
+            <Box terminal={[bash, user]} command={command} />;
+          </Col>
+        </Row>
+      ) : null}
     </Container>
   );
 };
@@ -66,7 +80,22 @@ const styles = {
     justifyContent: "'flex-start'",
   },
   terminalText: {
-    color: "rgb(0, 255, 0)",
+    color: "#70e224",
     fontFamily: "monospace",
+    padding: 5,
+    fontSize: 16,
+  },
+  terminalTextSpan: {
+    color: "#0b79fc",
+    fontSize: 16,
+  },
+  inputField: {
+    background: "transparent",
+    border: "none",
+    outline: "none",
+    caretColor: "#70e224",
+    color: "#70e224",
+    fontFamily: "monospace",
+    fontSize: 16,
   },
 };
