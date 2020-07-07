7dcdd679f4f1cdeb362b883866e308a8c2c98067
fse-dev <elmanoukdesign@gmail.com>
2020-07-06 20:51:13 -0400

    Updates

---
+++ precache-manifest.8e89b0b417393e7b98809d9a8188bf02.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a9e5796b4e2c4f3a51f76b29353da336",
+    "url": "/fserisen/index.html"
+  },
+  {
+    "revision": "ac7e6bf6bd9116f425f2",
+    "url": "/fserisen/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "cb8418c45db2d1beceac",
+    "url": "/fserisen/static/css/main.aaa9036c.chunk.css"
+  },
+  {
+    "revision": "ac7e6bf6bd9116f425f2",
+    "url": "/fserisen/static/js/2.577b265c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/fserisen/static/js/2.577b265c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cb8418c45db2d1beceac",
+    "url": "/fserisen/static/js/main.c5e3e7d5.chunk.js"
+  },
+  {
+    "revision": "be2028cf2dbbea84e2c7",
+    "url": "/fserisen/static/js/runtime-main.7ce18097.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/fserisen/precache-manifest.3bf692c8f44fea402482396cfd473a9f.js"
+  "/fserisen/precache-manifest.8e89b0b417393e7b98809d9a8188bf02.js"
 );
 
 self.addEventListener('message', (event) => {
