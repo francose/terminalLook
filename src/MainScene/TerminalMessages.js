1438edf32490cdb3fca9af5fa10302b26cce907d
francose <elmanoukdesign@gmail.com>
2020-08-09 16:45:35 -0400

    splash scene for the main site

--- src/MainScene/TerminalMessages.js
+++ src/MainScene/TerminalMessages.js
@@ -1,17 +1,35 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
 import CommandlineWelcomePart from "./commandline-header";
 import CommandlineOuputField from "./commandline-body";
 import TerminalInput from "./commandline-input";
+import Main from "./../WebSite/main"
+
+
+const splashScene = ()=>{
+  return <div>
+    <div >
+        <h1 className={"LoaderText"}>Please wait while loading...</h1>
+      </div>
+    <div className={"LoaderBox"}>
+       <div className={"loader"}></div>
+    </div>
+  </div>
+}
 
 export function TerminalMessages() {
+  const [loading, setloading] = useState(true)
   const sentTime = new Date().toLocaleTimeString();
   const sentDate = new Date().toLocaleDateString();
   const storeData = useSelector((state) => ({
     messages: state.messages,
     redirect: state.redirect
   }));
-  
+   useEffect(()=>{
+     setTimeout(()=>{
+      setloading(false)
+     }, 8500)
+  },[])
   return (
     storeData.redirect !== true?(
     <div >
@@ -24,11 +42,10 @@ export function TerminalMessages() {
         <TerminalInput time={sentTime} />
       </div>
     </div>):(
-      <div>
-        {/* online resume / website */}
-
-        <h1 style={{"color":"white"}}>{`WELCOME`}</h1>
-      </div>
+      !loading ? 
+      <Main />
+      :
+      splashScene()
     )
   );
 }
---
+++ src/WebSite/main.js
@@ -0,0 +1,12 @@
+import React from "react"
+
+
+const main=()=>{
+    return (
+        <div>
+         <h1 style={{"color":"white"}}>{`WELCOME -> Guest `}</h1>
+        </div>
+    )
+}
+
+export default main;
