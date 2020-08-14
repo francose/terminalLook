13690516a0d3d16891f95812eb887419fa2a249b
francose <elmanoukdesign@gmail.com>
2020-08-14 01:15:34 -0400

    get user connection

--- src/WebSite/splash.js
+++ src/WebSite/splash.js
@@ -1,25 +1,46 @@
 import React, { useEffect, useState } from "react"
 import Typewriter from 'typewriter-effect';
 
+const end_point = "http://localhost:5000/api/client"
+
 const Loader = ()=>{
   return <div className={"LoaderBox"}>
             <div className={"loader"}/>
         </div>
-
 }
 
+
 const Splash = (props)=>{
+  const [responseValue, setResponse] = useState({})
   const [flag, setFlag] = useState(true)
+  const checkState =  async(state)=>{
+    if (!state) return await fetch(end_point, 
+      {
+        method: 'POST', 
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({connection:true}),
+      })
+      .then(response => response.json())
+      .then(data => {
+        setResponse(data)
+      })
+    .catch((error) => {
+      console.error('Error:', error);
+    })
+    
+  }
     useEffect(()=>{
-        setInterval(()=>{
+       setInterval(()=>{
           setFlag(false)
-        }, 2500)
-        
-        return props.onChange(true)
-    },[])
+        }, 1500)
+        checkState(flag)
+        return  props.onChange(true)
+    },[flag])
 
     return <div style={{"display":"flex", "alignSelf":"center"}}>
-             <div style={{"width":400 }}>
+             <div style={{"width":400, "textAlign":"center" }}>
               <h3  >
                   <Typewriter
                       options={{
@@ -40,3 +61,5 @@ const Splash = (props)=>{
          </div>
 }
 export default Splash;
+
+
